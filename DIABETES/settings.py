191d8ccbb7d0c23e15c78fb97511a1c4b3f86454
Mahedi Hasan <mahediemon2001@gmail.com>
2026-05-20 22:37:49 +0600

    --

--- DIABETES/settings.py
+++ DIABETES/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-5#$zwqve)2rbvpbj+*t1bxru)*+ph6%ifeyy-zxhnxvbi*^kb=
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.onrender.com']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
